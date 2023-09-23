e58549dffcd573bf69f9acb09cec4dcf097c4aa5
yahir <s20120196@alumnos.itsur.edu.mx>
2023-09-23 13:04:16 -0600

    Se creo el package donde se agrego la clase Hero y HeroData con sus respectivos valores, y por ultimo se ñadio las cadenas con los nombre y descripcion de cada Heroe

---
+++ AppHeroes/app/src/main/java/com/juanyahirdr/appheroes/model/Hero.kt
@@ -0,0 +1,10 @@
+package com.juanyahirdr.appheroes.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Hero(
+    @StringRes val nameRes: Int,
+    @StringRes val descriptionRes: Int,
+    @DrawableRes val imageRes: Int
+)
